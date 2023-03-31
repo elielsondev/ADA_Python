83ebcd71da29cf65d8e0a07be2c29fdeac8c922c
Elielson Nascimento <nascimento.elielson@gmail.com>
2023-03-31 16:42:18 -0300

    feat - aprendendo input e print

---
+++ 01 - Entrada e Saída de Dados /primeiro.py
@@ -0,0 +1,12 @@
+# ETAPAS PARA A ENTRADA E SAÍDA DE DADOS:
+
+# Entrada de Dados / Ex.: Ingredientes
+linguagem = input(
+    'Qual é a linguagem de programação que você está praticando? '
+)
+
+# Processamento de Dados / Ex.: Receita
+mensagem = 'OK, você está praticando ' + linguagem
+
+# Saída de Dados / Ex.: Bolinho
+print(mensagem)
