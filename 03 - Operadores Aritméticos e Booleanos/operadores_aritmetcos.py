3d26bf243e91b988ba623061ccdd30be20d55063
Elielson Nascimento <nascimento.elielson@gmail.com>
2023-03-31 18:06:23 -0300

    feat - operadores aritmeticos e booleanos

---
+++ 03 - Operadores Aritméticos e Booleanos/operadores_aritmetcos.py
@@ -0,0 +1,23 @@
+# > Operações Matemáticas (Aritméticas)
+
+'''
+  - Soma: +
+  - Subtração: -
+  - Multiplicação: *
+  - Divisão: /
+  - Divisão inteira: //
+  - Resto ou Módulo: %
+  - Potência: **
+'''
+
+num1 = 10
+num2 = 2
+
+
+print(num1 + num2)  # Saída: 12
+print(num1 - num2)  # Saída: 8
+print(num1 * num2)  # Saída: 20
+print(num1 / num2)  # Saída: 5.0
+print(num1 // num2)  # Saída: 5
+print(num1 % num2)  # Saída: 0
+print(num1 ** num2)  # Saída: 100
---
+++ 03 - Operadores Aritméticos e Booleanos/operadores_booleanos.py
@@ -0,0 +1,15 @@
+# > Operadores Booleanos
+
+idade1 = 11
+idade2 = 7
+
+altura1 = 1.54
+altura2 = 1.25
+altura3 = altura2
+
+print(idade1 > idade2)  # True
+print(idade1 <= idade2)  # False
+print('Python' == 'python')  # False
+print('banana' != 'abacaxi')  # True
+print(altura1 >= altura2)  # True
+print(altura2 > altura3)  # False
