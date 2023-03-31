e6eea6f5752f12114ea6743b2dd407be4a7be6b7
Elielson Nascimento <nascimento.elielson@gmail.com>
2023-03-31 20:29:26 -0300

    feat - conversão de tipos

---
+++ 04 - Conversão de Tipos/conversao_de_tipos.py
@@ -0,0 +1,23 @@
+# Conversão de Tipos
+
+idade = '32'
+
+print(idade, type(idade))  # 32 <class 'str'>
+
+idade_inteira = int(idade)
+
+print(idade_inteira, type(idade_inteira))  # 32 <class 'int'>
+
+# int()
+# str()
+# float()
+# bool()
+
+
+altura = input('Informe a sua altura: ')
+
+print(altura, type(altura))  # 1.80 <class 'str'>
+
+altura2 = float(input('Informe a sua altura: '))
+
+print(altura2, type(altura2))  # 1.80 <class 'float'>
